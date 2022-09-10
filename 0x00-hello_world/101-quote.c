2a6ea5c6c6f9eb29e0a1c1952026104ee850c320
alemtideg <arsematebikgn@gmail.com>
2022-09-10 06:00:18 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - print the text
+ * main - print the size of variables
  *
- * Return: 1 if success.
+ * return: 1
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
-
