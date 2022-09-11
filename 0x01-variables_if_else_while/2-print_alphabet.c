7b8ec1b16f63acf37fa932b825bf0171b835dd68
alemtideg <arsematebikgn@gmail.com>
2022-09-11 13:24:22 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabetical order
+ *
+ * Description: main function
+ * this programming prints "alphabetical order
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a" ; ch <= 'z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
