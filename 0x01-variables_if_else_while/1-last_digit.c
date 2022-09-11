04a493339a9e9d2af08a9068a13b68e7f7c271af
alemtideg <arsematebikgn@gmail.com>
2022-09-11 12:57:02 -0700

    last digitt

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,11 +24,11 @@ if (n > 5)
 }
 else if (l == 0)
 {
-	printf("Last digit of %d is %d and is 0\", n, l);
+	printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else
 {
 	printf(Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 return (0);
-}
+i}
