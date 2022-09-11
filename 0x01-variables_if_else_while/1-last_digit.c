dac7d8f8e3166841b38b1d7f423d7c2bea39a04b
alemtideg <arsematebikgn@gmail.com>
2022-09-11 12:34:33 -0700

    task11111

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ else if (l == 1)
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 1);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 return (0);
 }
