3069667cd9d8b558461ff91f9dabd6c4cc49cf41
alemtideg <arsematebikgn@gmail.com>
2022-09-12 11:02:05 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,6 +11,7 @@ int main(void)
 {
 int num;
 for (num = 0; num <= 9; num++)
+{
 	putchar((num % 10) + '0');
 if (num == 9)
 	continue;
@@ -20,6 +21,5 @@ putchar(' ');
 putchar('\n');
 return (0);
 }
-
 
 
