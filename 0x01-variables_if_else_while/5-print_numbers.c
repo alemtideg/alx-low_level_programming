63eaf0850d38ba8079646e842dc98573f03c9caa
alemtideg <arsematebikgn@gmail.com>
2022-09-12 05:06:25 -0700

    sigle digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print function
+ *
+ * Description: main function
+ * this program prints "programming of single digit number
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
