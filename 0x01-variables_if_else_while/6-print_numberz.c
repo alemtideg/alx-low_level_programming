485a4310ea1471ea496df244ffbb09ab29748758
alemtideg <arsematebikgn@gmail.com>
2022-09-12 05:35:33 -0700

    single gigit using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * maain - print main fuction
+ *
+ * Description: main function
+ * this program prints "single digit base 10
+ * Return: 0
+ */
+int main()
+{
+int dig;
+for (dig = 0; dig < 10; dig++)
+	putchar((dig % 10) + '0');
+}
+putchar("\n");
+return (0);
+}
