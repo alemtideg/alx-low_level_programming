a0d8a41d5372cdfae5c33ec2876bf6a8cf2d416e
alemtideg <arsematebikgn@gmail.com>
2022-09-12 10:47:05 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>  
+
+/**
+ * main - print main function
+ *
+ * Description: main function
+ * this programprints "programing of combination of numbers up to 9
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+	putchar((num % 10) + '0');
+if (num == 9)
+continue;
+putchar(',');
+putchar('');
+}
+putchar('\n');
+return (0);
+}
+
+
+
