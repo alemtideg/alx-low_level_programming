5b36f374eff0352a27a35e130c22c57cdf2ce401
alemtideg <arsematebikgn@gmail.com>
2022-09-12 09:31:44 -0700

    task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,8 +14,8 @@ int dig;
 int letter;
 for (dig = 0; dig < 10; dig++)
 	putchar((dig % 10) +'0');
-for (letter = 'a'; letter <= 'f'; letter++);
-        putchar(letter);
+for (letter = 'a'; letter <= 'f'; letter++)
+	putchar(letter);
 putchar('\n');
 return (0);
 }
