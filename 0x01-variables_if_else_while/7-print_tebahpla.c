d1a4c4e7c1f624a4fc51c1e64b998141912edba3
alemtideg <arsematebikgn@gmail.com>
2022-09-12 06:05:24 -0700

    lower case in revers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print main function
+ *
+ * Description:: main function
+ * this program prints "programming of lower case in reverse
+ * Return: 0
+ */
+int main(void)
+{
+char(ch);
+for (ch = 'z'; ch <= 'a'; ch++)
+	putchar(ch);
+putchar('\n');
+return (0);
+}
