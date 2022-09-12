ca480bf7b443a71d20d451ae7893d652d98cce4f
alemtideg <arsematebikgn@gmail.com>
2022-09-12 09:15:13 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print main function
+ *
+ * Description: main function
+ * this program prints "programing of base 16 in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+int dig;
+int letter;
+for (dig = 0; dig < 10; dig++)
+	putchar((dig % 10) +'0');
+for (letter = 'a'; letter <= 'f'; letter++);
+        putchar(letter);
+putchar('\n');
+return (0);
+}
+
