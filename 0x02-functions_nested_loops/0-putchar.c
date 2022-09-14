37d909f5eca6fe2de90d5839fe1d32adfe3975f0
alemtideg <arsematebikgn@gmail.com>
2022-09-14 09:28:21 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - print the value
- *
- * Return: Always 0
+ * main - print _putchar
+ * Return: always 0
  */
 int main(void)
 {
-	printf"_putchar);
-printf('\n');
-return (0);
-}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,7 @@
-_putchar
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
+
+
+int _putchar(char c)
+
+#endif
