4cc08deb742462486ac96836094d21f45a211d3a
alemtideg <arsematebikgn@gmail.com>
2022-09-15 13:30:55 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,21 @@
+ifndef MY_HEADER_FILE_H                                                                                                                 
+#define MY_HEADER_FILE_H 
+
+/**
+ * main - header file for prototype
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
