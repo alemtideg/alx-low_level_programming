4a19542f54d2cb8158b9e19a79ad1d1e79fc040d
alemtideg <arsematebikgn@gmail.com>
2022-09-16 11:55:31 -0700

    task1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,9 +5,9 @@
  * @c: tested character
  * Return: 1 if c is digit othrwise return 0
  */
-int _isdigit(int c);
+int _isdigit(int c)
 {
-	if (c >= 48) &&(c <=57)
+	if (c >= 48) &&(c <= 57)
 		return (1);
 	else
 		return (0);
