282cbdba72ee05623e440e93bb8a17f47d0d69cb
alemtideg <arsematebikgn@gmail.com>
2022-09-16 12:47:10 -0700

    task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print - numbers 0 to 9
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int = 0;
+
+	while (n < 10)
+	{
+		_putchar(n + '0');
+		n += 1;
+	}
+	_putchar('\n')
+}
