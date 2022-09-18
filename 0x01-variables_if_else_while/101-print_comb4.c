8c855d91598f92f3a6eca2baab2168d3125bb0c7
alemtideg <arsematebikgn@gmail.com>
2022-09-18 06:25:44 -0700

    task11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,7 +18,6 @@ int main(void)
 				putchar((num1 % 10) + '0');
 				putchar((num2 % 10) + '0');
 				putchar((num3 % 10) + '0');
-				
 				if (num1 == 9 && num2 == 9 && num3 == 9)
 					continue;
 				putchar(',');
