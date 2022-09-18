b62b05f5e24484489cf448f094da17201f01a21d
alemtideg <arsematebikgn@gmail.com>
2022-09-18 06:15:51 -0700

    task11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num1,num2,num3;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for num2 = 0; num2 < 10; num2++)
+		{
+			for (num3 = 0; num3 < 10; num3++)
+			{
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
+				
+				if (num1 == 9 && num2 == 9 && num3 == 9)
+					continu;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	purchar('\n');
+	return (0);
+}
+
