68f150ea72b2629250c59fb9a61aff87f07ed064
alemtideg <arsematebikgn@gmail.com>
2022-09-18 05:17:34 -0700

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,13 +15,16 @@ int main(void)
 	{
 		putchar((num1 % 10) + '0');
 		putchar((num2 % 10) + '0');
+
 		if (num1 == 9 && num2 == 9)
 			continue;
+
 		putchar(',');
 		putchar(' ');
 	}
 	}
 	putchar('\n');
+
 	return (0);
 }
 
