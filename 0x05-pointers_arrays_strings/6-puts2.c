afcf9827de487b70cd5f4675479105b0664fc03f
alemtideg <arsematebikgn@gmail.com>
2022-09-19 14:33:01 -0700

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 - prints
+ * @str: prints
+ */
+void puts2(char *str)
+{
+	int index = 0, c =0;
+
+	while (str[index++])
+		c++;
+	for (index = 0; index < c; index += 2)
+		putchar(str[index]);
+
+	putchar('\n');
+}
