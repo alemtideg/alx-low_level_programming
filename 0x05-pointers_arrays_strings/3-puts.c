6f36914e7e485dfa25046163eb5605409529d380
alemtideg <arsematebikgn@gmail.com>
2022-09-19 13:00:30 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _put - print strings followed by new line
+ *
+ * @s: strings
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
+}
