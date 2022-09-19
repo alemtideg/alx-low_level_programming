2b9cbb6a3971b63f2e6e7d7c22373fbab0b95816
alemtideg <arsematebikgn@gmail.com>
2022-09-19 11:33:13 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of strings
+ * @c:
+ * @s:
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	 while(*s[c] != '\0')
+		 c++;
+	 return c;
+}
