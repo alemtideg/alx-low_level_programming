314248cafc00a4db909b6b44c26818442b24d317
alemtideg <arsematebikgn@gmail.com>
2022-09-21 10:19:26 -0700

    tsak7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,9 +14,9 @@
 	while (str[index] != '\0')
 		index++;
 
-	half = index / 2;
-	if (index % 2 == 1)       
-	       half++;
+half = index / 2;
+if (index % 2 == 1)      
+       	half++;
 	while (half < index)
 	{
 		_putchar(str[half]);
