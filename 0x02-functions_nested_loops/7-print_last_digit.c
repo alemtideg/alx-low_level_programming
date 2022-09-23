a93a295d10b270e69e481154e82e9a38325423bf
alemtideg <arsematebikgn@gmail.com>
2022-09-23 11:21:53 -0700

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of number N
+ * @n:last digit of n
+ * Return: the lat digit of n
+ */
+int print_last_digit(int);
+{
+	int last_digit = n % 10;
+
+	if (last_digit < 0)
+		last digit * = -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
+}
