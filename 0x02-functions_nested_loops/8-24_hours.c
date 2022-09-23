d9638502f7827716c5c6244f6753b603ae3a27d6
alemtideg <arsematebikgn@gmail.com>
2022-09-23 12:22:09 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * jack_bauer - print all minites of a day
- * 
+ * jack_bauer - print all minites of a day 
  * Return: return minutes of a day
  */
 void jack_bauer(void)
 {
 	int h, m;
-	for ( h = 0; h < 24; h++)
+
+	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			_putchar(h / 10 +'0');
+			_putchar(h / 10 + '0');
 			_putchar(h % 10 + '0');
 			_putchar(':');
 			_putchar(m / 10 + '0');
