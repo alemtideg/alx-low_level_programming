8a32c248e44b53ea4e1f52bcbfa060313735a953
alemtideg <arsematebkgn@gmail.com>
2022-09-27 08:33:45 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strch - print the function that locates a character in string
+ * @s: the string
+ * @c: the character c
+ *
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *(s + i); i++)
+	{
+		if (*(s + i) == c)
+			return (s + i);
+	}
+	if (*(s + i) == c) 
+		return (s + i);     
+	return (0);
+}
