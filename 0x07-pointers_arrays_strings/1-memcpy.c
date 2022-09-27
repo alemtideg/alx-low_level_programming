0bacc48bd9057fe23227848d7f4b383e0b14449b
alemtideg <arsematebkgn@gmail.com>
2022-09-27 07:51:03 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - prints memory area
+ * @dest: memory area
+ * @src: memory area
+ * @n: number of bytes
+ *
+ * Return: return pointers to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < 0; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
