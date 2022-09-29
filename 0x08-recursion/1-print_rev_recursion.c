926a7200de04457b7376226c1ae48a4a19f892ba
alemtideg <arsematebkgn@gmail.com>
2022-09-29 11:06:04 -0700

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _rev_recursion - print string in reverse
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+		_putchar(*s);
+	_print_rev_recursion(s + 1);
+}
+}
