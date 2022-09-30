900310c0662b4f1bbea96eb0ce1c8250d9a9c0b2
alemtideg <arsematebkgn@gmail.com>
2022-09-30 10:45:51 -0700

    main.h

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MY_HEADER_FILE_H             
+#define MY_HEADER_FILE_H      
+
+/**
+ *  main - header file for prototype
+ *  void prototypes (void) 
+ *  int prototypes (int) 
+ */
+int _putchar(char c);                                                                                                                         
+
+#endif             
