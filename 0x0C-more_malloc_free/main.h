5a79190e8e74526850417a3e83efd4877a53e007
alemtideg <arsematebkgn@gmail.com>
2022-10-06 09:04:46 -0700

    main.h

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,14 @@
+ifndef MY_HEADER_FILE_H                                                                                                                      
+#define MY_HEADER_FILE_H
+
+/**
+ * main - header file for prototype
+ * void prototypes (void)
+ * int prototypes (int)  
+ */
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+
+#endif
