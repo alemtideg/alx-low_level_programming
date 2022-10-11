e670a399c6328b9a880c77ef912fcc6718b4aba9
alemtideg <arsematebkgn@gmail.com>
2022-10-11 13:12:35 -0700

    task2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Write a program that prints the name of the file
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
