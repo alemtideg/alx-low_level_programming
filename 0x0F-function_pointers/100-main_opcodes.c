c7333f70a3d9084800894e1e26bed9f01a56dd77
alemtideg <arsematebkgn@gmail.com>
2022-10-12 10:49:14 -0700

    task4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the opcodes 
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int bytes, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+	printf("\n");
+	return (0);
+}
