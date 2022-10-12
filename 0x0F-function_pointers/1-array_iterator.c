bdc84235284eb96c3b4b91b1f63a596f0238207a
alemtideg <arsematebkgn@gmail.com>
2022-10-12 09:45:23 -0700

    task1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Write a function that executes a function given as a parameter
+ * array_iterator - Write a function that executes a function
  * @array: array
  * @size: size
  * @action: action
@@ -13,6 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (!array || !action)
 		return;
-	if (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
