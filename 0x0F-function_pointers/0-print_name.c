3d6ed8c1f23aff9dadfaec0d850d19ad2c1aecd4
alemtideg <arsematebkgn@gmail.com>
2022-10-12 09:22:39 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Write a function that prints a name.
+ * @name: name of the function
+ * @f: printing function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name ||!f)
+		return;
+	f(name);
+}
